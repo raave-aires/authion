8c5bc657827ad9100cd293233483eefce5525692
Raavë Aires <dev@raave.me>
2025-03-18 14:08:23 +0000

    adiciona links de navegação para "Entrar" e "Criar conta" no cabeçalho

--- components/structure/header.tsx
+++ components/structure/header.tsx
@@ -1,4 +1,6 @@
 // importações de componentes:
+import Link from "next/link";
+import { Button } from "../ui/button";
 import { Theme } from "../ui/theme-toggle";
 import { Logo } from "./logo";
 
@@ -7,7 +9,9 @@ export function Header(){
     <header className="sticky h-14 top-0 flex items-center bg-background/95 backdrop-blur-sm supports-backdrop-filter:bg-background/60 border-b border-border">
       <nav className="flex w-full items-center justify-between mx-8">
         <Logo />
-        <div>
+        <div className="flex gap-2 items-center justify-center">
+          <Button asChild><Link href="/conta/entrar">Entrar</Link></Button>
+          <Button variant="outline" asChild><Link href="/conta/criar">Criar conta</Link></Button>
           <Theme />
         </div>
       </nav>
