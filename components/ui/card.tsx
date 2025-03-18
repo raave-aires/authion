52391a9daf804ccd3d219298a2935e60fb06534b
Raavë Aires <dev@raave.me>
2025-03-18 14:31:55 +0000

    adiciona a classe "select-none" ao título do cartão para evitar seleção de texto

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -32,7 +32,7 @@ function CardTitle({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       data-slot="card-title"
-      className={cn("leading-none", className)}
+      className={cn("leading-none select-none", className)}
       {...props}
     />
   )
